6ac8b3dc2d007021025db577424ebf76e952d6e8
GithubJjang <rs60504@naver.com>
2023-11-17 20:07:18 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/demo13/TestController.kt
@@ -0,0 +1,13 @@
+package com.example.demo13
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController{
+
+    @GetMapping("/")
+    fun helloworld(): String{
+        return "I like koyeb";
+    }
+}
